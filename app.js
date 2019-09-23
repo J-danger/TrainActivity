5c1e4dd39c6064e759f68d099ea72558b1d8787d
Stephen Howe <Stevehowe10@gmail.com>
2019-09-23 16:23:34 -0600

    everything is working

--- app.js
+++ app.js
@@ -13,21 +13,22 @@
         var database = firebase.database();
 
         var time = "";
+        var trainName = "";
+        var destination = "";
+        var frequency = "";
+        var time = "";
+        var tRemainder = 0;
+        var tMinutesTillTrain = 0;
+        var tFrequency = 0;
+        var diffTime = 0;
 
 // inputs for firebase
 $("#add-train-btn").on("click", function(event) {
   event.preventDefault();
-        var trainName =$("#train-name-input").val().trim();
-        var destination = $("#destination-input").val().trim();
-        var frequency = $("#frequency-input").val().trim();
-        var time = $("#time-input").val().trim();
-
-// this needs to be calculated
-
-  console.log(trainName)
-  console.log(destination)
-//   console.log(time)
-  console.log(frequency)
+         trainName =$("#train-name-input").val().trim();
+         destination = $("#destination-input").val().trim();
+         frequency = $("#frequency-input").val().trim();
+         time = $("#time-input").val().trim();
 
     database.ref().push({
         name: trainName,
@@ -39,30 +40,55 @@ $("#add-train-btn").on("click", function(event) {
 
     })
 
-    // time calulation
-        var firsTime = time
-        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "weeks");
-        console.log(firstTimeConverted)
-    
     // firebase watcher + initial loader 
     database.ref().on("child_added", function(childSnapshot) {
-        
-        var nameDb = childSnapshot.val().name;
-        var destinationDb = childSnapshot.val().destination;
-        var frequencyDb = childSnapshot.val().frequency;
-        
-        // Log everything that's coming out of snapshot
-        console.log(nameDb);
-        console.log(destinationDb);
-        console.log(frequencyDb);
-
-        //  creates new rows with fb data
+      
+      var nameDb = childSnapshot.val().name;
+      var destinationDb = childSnapshot.val().destination;
+      var frequencyDb = childSnapshot.val().frequency;
+      var timeDb = childSnapshot.val().time;
+      
+      // Log everything that's coming out of snapshot
+      // console.log(nameDb);
+      // console.log(destinationDb);
+      // console.log(frequencyDb);
+      
+      tFrequency = frequencyDb;
+  
+      // Time is 3:30 AM
+      var firstTime = timeDb;
+  
+      // First Time (pushed back 1 year to make sure it comes before current time)
+      var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+      // console.log(firstTimeConverted);
+  
+      // Current Time
+      var currentTime = moment();
+      // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+  
+      // Difference between the times
+      diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      // console.log("DIFFERENCE IN TIME: " + diffTime);
+  
+      // Time apart (remainder)
+      tRemainder = diffTime % tFrequency;
+      // console.log(tRemainder);
+  
+      // Minute Until Train
+      tMinutesTillTrain = tFrequency - tRemainder;
+      // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+  
+      // Next Train
+      var nextTrain = moment().add(tMinutesTillTrain, "minutes").format('LT');
+      // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+      
+      //  creates new rows with fb data
         var newRow = $("<tr>").append(
             $("<td>").text(nameDb),
             $("<td>").text(destinationDb),
             $("<td>").text(frequencyDb),
-            // $("<td>").text(empMonths),
-            // $("<td>").text(empRate),
+            $("<td>").text(nextTrain),
+            $("<td>").text(tMinutesTillTrain),
             // $("<td>").text(empBilled)
           );
         
@@ -70,11 +96,7 @@ $("#add-train-btn").on("click", function(event) {
           $("#train-table > tbody").append(newRow);
         });
 
-      // Handle the errors
-    //  function(errorObject) {
-    //   console.log("Errors handled: " + errorObject.code);
-    // };
-
+   
    
 
 
